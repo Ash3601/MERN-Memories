a07418cc3e5882d2873d67562bebd908a3aaba12
Ash_360 <abbasiashin0@gmail.com>
2021-12-04 12:11:18 +0530

    Created basic components now starting to add styles in App.js first

--- memories_project/server/routes/posts.js
+++ memories_project/server/routes/posts.js
@@ -1,8 +1,8 @@
 import express from "express";
-import { getPosts, createPost } from "../controllers/posts.controller.js";
+import { getPosts, createPosts } from "../controllers/posts.controller.js";
 const router = express.Router();
 
 router.get("/", getPosts);
-router.post("/", createPost);
+router.post("/", createPosts);
 
 export default router;
