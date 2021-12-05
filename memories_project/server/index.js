36c01474a796efb56875ef0b8db93aa8e52f9252
Ash_360 <abbasiashin0@gmail.com>
2021-12-06 02:53:21 +0530

    Able to store data to database and also retrieve the data

--- memories_project/server/index.js
+++ memories_project/server/index.js
@@ -6,10 +6,10 @@ import postRoutes from "./routes/posts.js";
 
 const app = express();
 
-app.use("/posts", postRoutes);
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
+app.use("/posts", postRoutes);
 
 //TODO: will be put to env variable later
 const CONNECTION_URL =
